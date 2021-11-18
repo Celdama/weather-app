03437436d803e710975c76097dc7fe2e7375bd29
Celdama <celdama.dev@gmail.com>
2021-11-18 08:09:43 +0100

    added function and style to display temperature forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import getHours from 'date-fns/getHours';
+
 const weatherApp = (() => {
   const displayLocalisation = document.querySelector('.localisation');
   const displayTemp = document.querySelector('.weather-deg');
@@ -7,10 +9,63 @@ const weatherApp = (() => {
   const displayHumidity = document.querySelector('.humidity-info');
   const displayWind = document.querySelector('.wind-info');
   const displayRain = document.querySelector('.rain-info');
+  const forecastBar = document.querySelectorAll('.prev');
+  const currentBar = document.querySelector('.current');
+  const displayCurrentHour = document.querySelector('.current-hour');
+  const displayCurrentTemp = document.querySelector('.current-temp');
+  const displayForecastTemp = document.querySelectorAll('.forecast-temp');
+
+  const convertForecastTempInPercent = (listOfTemp, higherTemp) => {
+    const percent = [];
+
+    listOfTemp.forEach((temp) => {
+      percent.push((temp * 100) / higherTemp);
+    });
+
+    return percent;
+  };
+
+  const renderForecastData = (data, currentTime, currentTemp) => {
+    const higherDayTemp = data.reduce((p, v) => (p > v ? p : v));
+    let currentHour = getHours(new Date(currentTime));
+    const forecastTempForNext6Hours = [];
+
+    data.forEach((temp, i) => {
+      let dataPush = i;
+
+      if (dataPush < 7) {
+        forecastTempForNext6Hours.push(data[currentHour]);
+      }
+
+      dataPush += 1;
+
+      currentHour += 1;
+      if (currentHour > 23) {
+        currentHour = 0;
+      }
+    });
+
+    displayCurrentHour.textContent = currentHour > 12
+      ? `${currentHour}PM` : `${currentHour}AM`;
+
+    displayCurrentTemp.innerHTML = `${Math.round(currentTemp)}&#176`;
+
+    console.log(currentBar);
+    currentBar.style.height = `${(currentTemp * 100) / higherDayTemp}%`;
+    // console.log((currentTemp * 100) / higherDayTemp);
+
+    const percentValue = convertForecastTempInPercent(forecastTempForNext6Hours, higherDayTemp);
+
+    forecastBar.forEach((bar, i) => {
+      bar.style.height = `${percentValue[i]}%`;
+    });
+
+    displayForecastTemp.forEach((temp, i) => {
+      temp.innerHTML = `${Math.round(forecastTempForNext6Hours[i])}&#176`;
+    });
+  };
 
   const renderWeatherData = (currentData, forecastData) => {
-    console.log(forecastData);
-
     const {
       name, tempC, localtime, text, cloud, humidity,
       wind, precip, tempF,
@@ -26,6 +81,8 @@ const weatherApp = (() => {
     displayHumidity.innerHTML = `${humidity}%`;
     displayWind.innerHTML = `${wind}km/h`;
     displayRain.innerHTML = `${precip}mm`;
+
+    renderForecastData(forecastData, localtime, tempC);
   };
 
   const processWeatherInfo = (info) => {
@@ -63,6 +120,7 @@ const weatherApp = (() => {
     try {
       const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=1&aqi=no&alerts=no`);
       response.json().then((res) => {
+        console.log(res);
         const { currentDayData, forecastDayData } = processWeatherInfo(res);
         renderWeatherData(currentDayData, forecastDayData);
       });
@@ -77,5 +135,5 @@ const weatherApp = (() => {
 })();
 
 window.onload = () => {
-  // weatherApp.getWeather('new york');
+  weatherApp.getWeather('paris');
 };
