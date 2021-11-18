2e672d53032b2edb10b2b2d5e1d112657b70140d
Celdama <celdama.dev@gmail.com>
2021-11-18 12:56:08 +0100

    add dynamic background

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ const weatherApp = (() => {
   const displayForecastTemp = document.querySelectorAll('.forecast-temp');
   const displayForecastHour = document.querySelectorAll('.forecast-hour');
   const form = document.querySelector('form');
+  const app = document.querySelector('#app');
+  const main = document.querySelector('main');
 
   const convertForecastTempInPercent = (listOfTemp, higherTemp) => {
     const percent = [];
@@ -84,10 +86,35 @@ const weatherApp = (() => {
     });
   };
 
+  const changeBackgroundImage = (codeWeatherCondition) => {
+    // code from API
+    const sunnyCode = [1000];
+    const cloudCode = [1003, 1009, 1030];
+    const rainCode = [1063, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1240, 1246];
+    const snowCode = [1066, 1069, 1072, 1144, 1117, 1213, 1225, 1222, 1219];
+
+    if (sunnyCode.includes(codeWeatherCondition)) {
+      app.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1586078074298-05dca4848695?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80\')';
+      main.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1586078074298-05dca4848695?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80\')';
+    } else if (cloudCode.includes(codeWeatherCondition)) {
+      app.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1532178910-7815d6919875?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80\')';
+      main.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1532178910-7815d6919875?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80\')';
+    } else if (rainCode.includes(codeWeatherCondition)) {
+      app.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1509635022432-0220ac12960b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80\')';
+      main.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1509635022432-0220ac12960b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80\')';
+    } else if (snowCode.includes(codeWeatherCondition)) {
+      app.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1515106426117-7483c8b91e9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80\')';
+      main.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1515106426117-7483c8b91e9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80\')';
+    } else {
+      app.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1560461311-0535e8fa1ba5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80\')';
+      main.style.backgroundImage = 'url(\'https://images.unsplash.com/photo-1560461311-0535e8fa1ba5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80\')';
+    }
+  };
+
   const renderWeatherData = (currentData, forecastData) => {
     const {
       name, tempC, localtime, text, cloud, humidity,
-      wind, precip, tempF,
+      wind, precip, tempF, code,
     } = currentData;
 
     displayLocalisation.textContent = name;
@@ -100,6 +127,8 @@ const weatherApp = (() => {
     displayRain.innerHTML = `${precip}mm`;
 
     renderForecastData(forecastData, localtime, tempC);
+
+    changeBackgroundImage(code);
   };
 
   const processWeatherInfo = (info) => {
@@ -116,6 +145,7 @@ const weatherApp = (() => {
       precip: current.precip_mm,
       tempC: current.temp_c,
       tempF: current.temp_f,
+      code: current.condition.code,
     };
 
     const forecastDayDataWithHour = info.forecast.forecastday[0].hour;
